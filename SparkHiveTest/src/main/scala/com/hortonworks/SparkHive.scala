d4f5ae6567a7da85f996d66f57cafb443039746d
Hari Gopal Matta <hmatta@HW15669.local>
2019-10-25 20:22:18 +0530

    Spark Hive Example

---
+++ SparkHiveTest/src/main/scala/com/hortonworks/SparkHive.scala
@@ -0,0 +1,28 @@
+package com.hortonworks
+import org.apache.spark.sql.SparkSession
+import org.apache.log4j._
+
+object SparkHive {
+
+  def main(args: Array[String]): Unit =
+  {
+    val spark =  SparkSession.builder().appName("SparkHiveIntegration")
+    .enableHiveSupport().getOrCreate()
+
+    val logger = LogManager.getRootLogger
+
+    logger.info("creating table test")
+
+    spark.sql("create table test(id int)")
+
+    logger.info("Inserting data to table test")
+
+    spark.sql("insert into table test values(100)")
+    spark.sql( sqlText = "insert into table test values(200)")
+
+    logger.info("Projecting data from table test")
+
+    spark.sql("select * from test").show()
+  }
+
+}
